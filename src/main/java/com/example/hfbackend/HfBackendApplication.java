5c05f6120b5b4621a0c8eda1f9f86d6ee892619a
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-11-29 15:45:43 +0100

    Rasmus

--- src/main/java/com/example/hfbackend/HfBackendApplication.java
+++ src/main/java/com/example/hfbackend/HfBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HfBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HfBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HfBackendApplication.class, args);
+    }
 
 }
