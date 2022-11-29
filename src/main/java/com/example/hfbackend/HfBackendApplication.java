7e7505690d1aa01258e6abffe79d8a16bf6c1dc2
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-11-29 14:23:11 +0100

    projekt start

---
+++ src/main/java/com/example/hfbackend/HfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HfBackendApplication.class, args);
+	}
+
+}
